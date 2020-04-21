00acb5f74e915cf4fb307f5459dbaebb94eb1efa
Jonathan T Dean <jonathantomdean@gmail.com>
2020-04-21 23:01:42 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,11 @@
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+  # array.index(value_to_find)
+  
+  i = 0 
+  while i < array.length
+    if array[i] == value_to_find
+      return i
+    end
+    i += 1
+  end
 end
